4b3ed80e28c5894b6eb5faff41c9f6acfa0bda99
Henry <hhen5562@gmail.com>
2024-06-27 01:52:10 +0000

    "Updated Projects component with new image hover effect and added PopModel component with image display"

--- mportfolio/src/design/popModel.jsx
+++ mportfolio/src/design/popModel.jsx
@@ -1,4 +1,6 @@
 import { useRef } from "react";
+
+import { projects } from "../constant/project"; 
 import { IoClose } from "react-icons/io5";
 
 
@@ -10,13 +12,15 @@ const PopModel = ({onClose}) => {
             onClose()
         }
     }
+const img = {projects};
 
     return (
       <div ref={modelRef} onClick={closeModel}className="fixed w-screen h-screen z-50 top-0 left-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center">
         <div className="mt-10 flex flex-col ">
           <button onClick={onClose} className="place-self-end"><IoClose size={30} /></button>
   
-          <div className="mx-11 h-[700px] bg-slate-600 rounded-lg p-4">
+          <div className="mx-11 h-[700px] bg-gray-700 rounded-lg">
+            <img src={projects[0].img} alt="image" className="w-fit rounded-lg"/>
               <h1 className="text-xl font-black">PopModel</h1>
               <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.
